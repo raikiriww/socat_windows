767db428d2b8df8f478d91b8e1d34ea09fcf1708
raikiriww <ww@wwcommon.lan>
2024-02-26 12:19:19 +0000

    use wget

--- build-script.sh
+++ build-script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -o socat-1.8.0.0.tar.gz 'http://www.dest-unreach.org/socat/download/socat-1.8.0.0.tar.gz'
+wget http://www.dest-unreach.org/socat/download/socat-1.8.0.0.tar.gz
 tar -xvzf socat-1.8.0.0.tar.gz
 cd socat-1.8.0.0
 ./configure
