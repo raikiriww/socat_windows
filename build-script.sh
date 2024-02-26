7854a89c65608a630007c67e08954ac76591ee41
raikiriww <ww@wwcommon.lan>
2024-02-26 12:15:00 +0000

    chang curl url

--- build-script.sh
+++ build-script.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -O http://www.dest-unreach.org/socat/download/socat-1.8.0.0.tar.gz
+curl -o socat-1.8.0.0.tar.gz 'http://www.dest-unreach.org/socat/download/socat-1.8.0.0.tar.gz'
 tar -xvzf socat-1.8.0.0.tar.gz
 cd socat-1.8.0.0
 ./configure
