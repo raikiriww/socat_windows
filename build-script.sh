196c3bdf58796d1706f7f62656dcdcc5f1de5817
raikiriww <ww@wwcommon.lan>
2024-02-26 12:06:39 +0000

    test

--- build-script.sh
+++ build-script.sh
@@ -1,19 +1,7 @@
 #!/bin/bash
-
-# 下载源码压缩包
-curl -o socat-1.8.0.0.tar.gz http://www.dest-unreach.org/socat/download/socat-1.8.0.0.tar.gz
-
-# 解压源码
+curl -O http://www.dest-unreach.org/socat/download/socat-1.8.0.0.tar.gz
 tar -xvzf socat-1.8.0.0.tar.gz
-
-# 进入解压后的目录
 cd socat-1.8.0.0
-
-# 配置
 ./configure
-
-# 编译
 make
-
-# 安装
 make install
